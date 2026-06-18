299e8def14ecdbd4fd4fc213f8013c095431380e
arianavvqq13-star <ariana.vv.qq13@gmail.com>
2026-06-18 11:38:42 -0500

    semana 13

---
+++ SEMANA 16/NodoArbol.java
@@ -0,0 +1,15 @@
+
+package Libreria;
+
+
+public class NodoArbol {
+    int dato;
+    NodoArbol izquierda;
+    NodoArbol derecha;
+
+    public NodoArbol(int dato) {
+        this.dato = dato;
+        izquierda = null;
+        derecha = null;
+    }
+}
