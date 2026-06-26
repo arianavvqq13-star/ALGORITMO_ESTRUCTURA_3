c0837402a4da343d5ccba358fbecdc0624d67641
arianavvqq13-star <ariana.vv.qq13@gmail.com>
2026-06-26 13:11:52 -0500

    semana 14 lab

---
+++ SEMANA 14/Nodo.java
@@ -0,0 +1,14 @@
+package Libreria;
+
+public class Nodo {
+
+    int dato;
+    Nodo izquierdo;
+    Nodo derecho;
+
+    public Nodo(int dato) {
+        this.dato = dato;
+        this.izquierdo = null;
+        this.derecho = null;
+    }
+}
